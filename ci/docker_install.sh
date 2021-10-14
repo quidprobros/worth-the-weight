ddb208cf6dae0fbc0cf6ecff423996fc00b1748d
Chris <7539871+paxperscientiam@users.noreply.github.com>
2021-10-13 21:50:29 -0400

    bump

--- ci/docker_install.sh
+++ ci/docker_install.sh
@@ -10,11 +10,6 @@ set -xe
 apt-get update -yqq
 apt-get install git apt-utils -yqq
 
-mkdir vendor/
 
-wget https://composer.github.io/installer.sig -O - -q | tr -d '\n' > installer.sig
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('SHA384', 'composer-setup.php') === file_get_contents('installer.sig')) { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-php composer-setup.php
-php -r "unlink('composer-setup.php'); unlink('installer.sig');"
-php composer.phar install
+curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+
