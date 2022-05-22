7ee09cc4c4ab75ff415e541ba07e3906f904145d
Chris <7539871+paxperscientiam@users.noreply.github.com>
2022-05-22 19:23:46 -0400

    handle vitals form when settings not set

--- Application/app/Models/MeasurementUnits.php
+++ Application/app/Models/MeasurementUnits.php
@@ -6,6 +6,18 @@ class MeasurementUnits extends \Illuminate\Database\Eloquent\Model
 {
     protected $table = "measurement_unit_type";
 
+    protected $guarded = [
+        "id",
+        "measurement_parameter",
+        "unit_name",
+        "unit_name_singular",
+        "unit_name_abbreviation",
+    ];
+
+    protected $fillable = [];
+
+
+
     public function heights()
     {
         return $this->where("measurement_parameter", "=", "height")->get();
--- Application/app/Models/UserSettings.php
+++ Application/app/Models/UserSettings.php
@@ -21,13 +21,11 @@ class UserSettings extends \Illuminate\Database\Eloquent\Model
 
     public function height_unit()
     {
-        return $this->belongsTo(MeasurementUnits::class, "height_unit_id")
-                    ->withDefault();
+        return $this->belongsTo(MeasurementUnits::class, "height_unit_id");
     }
 
     public function weight_unit()
     {
-        return $this->belongsTo(MeasurementUnits::class, "weight_unit_id")
-                    ->withDefault();
+        return $this->belongsTo(MeasurementUnits::class, "weight_unit_id");
     }
 }
